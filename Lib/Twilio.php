a33ee9b02b2e67af2b696015ec7f11a58500171b
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2015-05-06 13:41:56 +0600

    Added call and generate TwiML functionality

--- Lib/Twilio.php
+++ Lib/Twilio.php
@@ -1,5 +1,14 @@
 <?php
 require_once ROOT . DS . 'Vendor' . DS . 'twilio' . DS . 'sdk' . DS . 'Services' . DS . 'Twilio.php';
+require_once ROOT . DS . 'Vendor' . DS . 'twilio' . DS . 'sdk' . DS . 'Services' . DS . 'Twilio' . DS 'Twiml.php';
+/**
+ * Twilio
+ * 
+ * @property string|null $__accountSid
+ * @property string|null $__authToken
+ * @property Services_Twilio|null $__instance 
+ * 
+ */
 class Twilio {
 
 
@@ -7,11 +16,25 @@ class Twilio {
     private $__authToken = null;
     private $__instance = null;
     
-
+/**
+ * Twilio object constructor
+ * 
+ * @param string|null $accountSid account secret id
+ * @param string|null $authToken  account auth token
+ * 
+ */
     public function __construct($accountSid = null, $authToken = null) {
         $this->getInstance($accountSid, $authToken);
     }
 
+/**
+ * getInstance method
+ * 
+ * @param  string|null $accountSid account secret id
+ * @param  string|null $authToken  account auth token
+ * @return Services_Twilio|null    Twilio API object or null
+ * 
+ */
     public function getInstance($accountSid = null, $authToken = null) {
         if (!empty($accountSid) && !empty($authToken)) {
             $this->__accountSid = $accountSid;
@@ -21,6 +44,14 @@ class Twilio {
         return $this->__instance;
     }
 
+/**
+ * getUniqueCode - Generates unique code for sms
+ * 
+ * @param  integer $len     generated code length
+ * @param  string  $symbols allowed symbols string
+ * @return string
+ * 
+ */
     public function getUniqueCode($len = 4, $symbols = "0123456789") {
         $code = '';
         for ($i = 0; $i < $len; $i++) {
@@ -30,6 +61,15 @@ class Twilio {
     }
 
 
+/**
+ * sendSMS - Sends sms
+ * 
+ * @param  string $from - sms from phone number
+ * @param  string $to   - sms ro phone number
+ * @param  string $text - sms message text 
+ * @return int
+ * 
+ */
     public function sendSMS($from, $to, $text) {
         $messageId = $this->__instance->account->messages->create(array(
                 'From' => $from,
@@ -39,7 +79,48 @@ class Twilio {
         return $messageId;
     }
 
-    public function call($from, $to) {
+/**
+ * generateTwiML - Generates TwiML with passed text and music from url
+ * 
+ * @param  string $text          - call message text 
+ * @param  string|null $musicUrl - music url 
+ * @return string                - generated XML
+ * 
+ */
+    public function generateTwiML($text, $musicUrl = null, $musicOptions = array()) {
+        $response = new Services_Twilio_Twiml();
+        $response->say($text);
+        if (!empty($musicUrl)) {
+            $response->play($musicUrl, $musicOptions);
+        }
+        return $response->__toString();
+    }
 
+/**
+ * call - Maked a call with passed twiML
+ * 
+ * @param  string $from  - call from phone number
+ * @param  string $to    - call to phone number
+ * @param  string $twiMl - TwiML url
+ * @return integer
+ * 
+ */
+    public function call($from, $to, $twiMl) {
+        $callId = null;
+        
+        if (empty($from) || empty($to) || empty($twiMl)) {
+            return null;
+        }
+
+        try {
+            $callId = $this
+                ->__instance
+                ->account
+                ->calls
+                ->create($from, $to, $twiML);
+        } catch (Services_Twilio_RestException $e) {
+
+        }
+        return $callId;
     }
 }
